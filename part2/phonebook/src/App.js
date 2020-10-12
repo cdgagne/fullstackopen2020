77416b9e2d2d5a58102f18494a9a48bc7b1aff68
Chris Gagne <cdgagne@gmail.com>
2020-10-12 11:23:34 -0700

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({ person }) => <li>{person.name} {person.number}</li>
 
@@ -32,15 +33,18 @@ const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, 
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+
+  const hook = () => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }
+  useEffect(hook, [])
 
   const handleAdd = (event) => {
     event.preventDefault()
