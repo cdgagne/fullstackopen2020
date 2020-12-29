223a2ff6247726a5232ede94fa82d3d2bb55140b
Chris Gagne <cdgagne@gmail.com>
2020-12-29 12:06:20 -0800

    Exercise 4.13 and 4.14 Blog list expansions, step1 and step2

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -20,4 +20,22 @@ blogsRouter.post('/', async (request, response, next) => {
     response.status(201).json(result)
 })
 
+blogsRouter.delete('/:id', async (request, response, next) => {
+    const result = await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).json(result)
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+    const body = request.body
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    }
+
+    const result = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.status(200).json(result)
+})
+
 module.exports = blogsRouter
--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -14,81 +14,125 @@ beforeEach(async () => {
     await Promise.all(promiseArray)
 })
 
-test('the correct number of blogs are returned as json', async () => {
-    const response = await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
+describe('viewing a blog', () => {
+    test('the correct number of blogs are returned as json', async () => {
+        const response = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('a blog has a unique identifier named id', async () => {
+        const blogs = await helper.blogsInDb()
+
+        expect(blogs[0].id).toBeDefined()
+    })
 })
 
-test('a blog has a unique identifier named id', async () => {
-    const blogs = await helper.blogsInDb()
+describe('adding a blog', () => {
+    test('a valid blog can be added', async () => {
+        const blog = {
+            title: 'Python creator Guido van Rossum joins Microsoft',
+            author: 'Frederic Lardinois',
+            url: 'https://techcrunch.com/2020/11/12/python-creator-guido-van-rossum-joins-microsoft/',
+            likes: 48,
+        }
 
-    expect(blogs[0].id).toBeDefined()
+        await api
+            .post('/api/blogs')
+            .send(blog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+        const titles = blogsAtEnd.map(b => b.title)
+        expect(titles).toContain(blog.title)
+    })
+
+    test('if a blog added is missing the likes property it will default to 0', async () => {
+        const blog = {
+            title: 'Python creator Guido van Rossum joins Microsoft',
+            author: 'Frederic Lardinois',
+            url: 'https://techcrunch.com/2020/11/12/python-creator-guido-van-rossum-joins-microsoft/'
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(blog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const addedBlogs = await Blog.find({ title: blog.title })
+        expect(addedBlogs[0].likes).toEqual(0)
+    })
+
+    test('if a blog added is missing the title or url the HTTP status code will be 400 Bad Request', async () => {
+        const blog = {
+            title: 'Python creator Guido van Rossum joins Microsoft',
+            author: 'Frederic Lardinois',
+            url: 'https://techcrunch.com/2020/11/12/python-creator-guido-van-rossum-joins-microsoft/'
+        }
+
+        let blogWithoutTitle = blog
+        delete blogWithoutTitle.title
+
+        await api
+            .post('/api/blogs')
+            .send(blog)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        let blogWithoutUrl = blog
+        delete blogWithoutUrl.url
+
+        await api
+            .post('/api/blogs')
+            .send(blog)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+    })
 })
 
-test('a valid blog can be added', async () => {
-    const blog = {
-        title: 'Python creator Guido van Rossum joins Microsoft',
-        author: 'Frederic Lardinois',
-        url: 'https://techcrunch.com/2020/11/12/python-creator-guido-van-rossum-joins-microsoft/',
-        likes: 48,
-    }
+describe('deleting a blog', () => {
+    test('delete a blog entry with a valid id', async () => {
+        const blogs = await helper.blogsInDb()
+        const blogToDelete = blogs[0]
 
-    await api
-        .post('/api/blogs')
-        .send(blog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
 
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-    const titles = blogsAtEnd.map(b => b.title)
-    expect(titles).toContain(blog.title)
+        const blogsAfterDelete = await helper.blogsInDb()
+        const blogIds = blogsAfterDelete.map(b => b.id)
+        expect(blogIds).not.toContain(blogToDelete.id)
+    })
 })
 
-test('if a blog added is missing the likes property it will default to 0', async () => {
-    const blog = {
-        title: 'Python creator Guido van Rossum joins Microsoft',
-        author: 'Frederic Lardinois',
-        url: 'https://techcrunch.com/2020/11/12/python-creator-guido-van-rossum-joins-microsoft/'
-    }
+describe('updating a blog', () => {
+    test('update a blog entry with a valid id', async () => {
+        const blogs = await helper.blogsInDb()
+        const blogToUpdate = blogs[0]
 
-    await api
-        .post('/api/blogs')
-        .send(blog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        const newBlogEntry = {
+            title: 'An updated blog',
+            author: 'Joey Jo Jo Junior Shabadoo',
+            url: 'http://www.asdf.com',
+            likes: 123,
+        }
 
-    const addedBlogs = await Blog.find({ title: blog.title })
-    expect(addedBlogs[0].likes).toEqual(0)
-})
+        await api
+            .put(`/api/blogs/${blogToUpdate.id}`)
+            .send(newBlogEntry)
+            .expect(200)
 
-test('if a blog added is missing the title or url the HTTP status code will be 400 Bad Request', async () => {
-    const blog = {
-        title: 'Python creator Guido van Rossum joins Microsoft',
-        author: 'Frederic Lardinois',
-        url: 'https://techcrunch.com/2020/11/12/python-creator-guido-van-rossum-joins-microsoft/'
-    }
-
-    let blogWithoutTitle = blog
-    delete blogWithoutTitle.title
-
-    await api
-        .post('/api/blogs')
-        .send(blog)
-        .expect(400)
-        .expect('Content-Type', /application\/json/)
-
-    let blogWithoutUrl = blog
-    delete blogWithoutUrl.url
-
-    await api
-        .post('/api/blogs')
-        .send(blog)
-        .expect(400)
-        .expect('Content-Type', /application\/json/)
+        const blogAfterUpdate = await Blog.findById(blogToUpdate.id)
+        expect(blogAfterUpdate.title).toEqual(newBlogEntry.title)
+        expect(blogAfterUpdate.author).toEqual(newBlogEntry.author)
+        expect(blogAfterUpdate.url).toEqual(newBlogEntry.url)
+        expect(blogAfterUpdate.likes).toEqual(newBlogEntry.likes)
+    })
 })
 
 afterAll(() => {
