105014bda4330db0518139cbcecb54180c00912a
Chris Gagne <cdgagne@gmail.com>
2021-01-01 16:42:30 -0800

    Exercise 4.19 bloglist expansion, step7

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -9,6 +9,8 @@ const errorHandler = (error, request, response, next) => {
         return response.status(400).send({ error: 'malformed id' })
     } else if (error.name === 'ValidationError') {
         return response.status(400).send({ error: error.message })
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).send({ error: 'invalid or missing token' })
     }
 
     next(error)
