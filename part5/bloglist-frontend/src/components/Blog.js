b829bdd528456f503125f38ff888c297c12263f8
Chris Gagne <cdgagne@gmail.com>
2021-01-25 13:35:37 -0800

    Exercise 5.7 Blog list frontend, step7 - Add view/hide button to Blog component

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+      <div style={showWhenVisible}>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          <p>{blog.author}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
