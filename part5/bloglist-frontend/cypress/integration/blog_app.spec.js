85b5659dd2661bc9597cf42d067156a6acc5e8e6
Chris Gagne <cdgagne@gmail.com>
2021-02-27 16:12:50 -0800

    Exercise 5.18 bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Test User',
+      username: 'testuser',
+      password: 'testpassword'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -10,5 +16,22 @@ describe('Blog app', function() {
     cy.get('input#username')
     cy.get('input#password')
     cy.get('button#login-button')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input#username').type('testuser')
+      cy.get('input#password').type('testpassword')
+      cy.get('button#login-button').click()
+      cy.contains('Test User is logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input#username').type('testuser')
+      cy.get('input#password').type('wrongpassword')
+      cy.get('button#login-button').click()
+      cy.get('.error').should('contain', 'Wrong username or password')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
