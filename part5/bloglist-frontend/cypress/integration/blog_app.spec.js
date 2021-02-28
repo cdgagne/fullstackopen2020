97dd528b88b340665e97dfa4ffa8890e99760c4f
Chris Gagne <cdgagne@gmail.com>
2021-02-27 16:26:18 -0800

    Exercise 5.19 bloglist end to end testing, step3

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -34,4 +34,26 @@ describe('Blog app', function() {
       cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
     })
   })
+
+  describe.only('When logged in', function() {
+    beforeEach(function() {
+      cy.login({ username: 'testuser', password: 'testpassword' })
+    })
+
+    it('A blog can be created', function() {
+      // Can enter a new blog
+      cy.contains('New Blog').click()
+      cy.get('input#blogTitle').type('A New Blog Entry')
+      cy.get('input#blogAuthor').type('Some Authors Name')
+      cy.get('input#blogUrl').type('http://the-blog-url')
+      cy.contains('Create').click()
+
+      // A notification is displayed
+      cy.get('.info').should('contain', 'A new blog \'A New Blog Entry\' by Some Authors Name added')
+      cy.get('.info').should('have.css', 'color', 'rgb(0, 128, 0)')
+
+      // The blog entry is in the list of blogs
+      cy.get('.blogTitleAndAuthor').should('contain', 'A New Blog Entry Some Authors Name')
+    })
+  })
 })
