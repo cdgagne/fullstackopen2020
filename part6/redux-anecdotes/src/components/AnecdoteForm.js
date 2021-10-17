1136381a851cabe02145ec1728aa9c68d26264ea
Chris Gagne <cdgagne@gmail.com>
2021-10-17 16:06:49 -0700

    Exercise 6.14 Anecdotes and the backend, step2 - Store newly created anecdotes in the json-server backend

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { createNotification, dismissNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
 
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
+
     const notificationStr = `you created '${content}'`
     dispatch(createNotification(notificationStr))
     setTimeout(() => {
